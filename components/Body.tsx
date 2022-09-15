dc923526f07faecf1a0ae1538b22c7d061303768
amithamurukesh <109946862+amithamurukesh@users.noreply.github.com>
2022-09-15 14:50:52 +0530

    Updated

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,29 +1,73 @@
+import React from "react";
+import axios from "axios";
 import { FunctionComponent } from "react";
 import Button from "./Button";
-
+import Header from "./Header";
+import Footer from "./Footer";
+import Image from "next/image";
 interface BodyProps {
   title: string;
 }
 
 const Body: FunctionComponent<BodyProps> = ({ title }) => {
+  const baseURL = "http://localhost:3001/products";
+
+  const [blog, setBlog] = React.useState(null);
+  const [isLoading, setIsloading] = React.useState(false);
+  const config = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    },
+  };
+  React.useEffect(() => {
+    setIsloading(true);
+    axios.get(baseURL, config).then(
+      (response) => {
+        setBlog(response.data);
+        setIsloading(false);
+      }
+      
+    );
+  }, []);
+  {
+    console.log(blog);
+    console.log(1);
+  }
   return (
-    <section className="py-16">
-      <div className="container mx-auto md:px-20">
-        <h1 className="font-bold text-3xl pb-12 text-center">{title}</h1>
+    <div className="h-screen">
+      <Header />
 
-        {/* <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
-    <div><a className="text-orange-600 hover:text-orange-800">Trivandrum</a> */}
+      <section className="py-16 bg-my_bg_image h-[70%] sm:h-[80%] md:h-[82%]">
 
-        <div className=" md:w-1/3 h-50"></div>
-        <div className="bg-gray-300 w-full md:2/3 p-8">
-          <h2 className="text-2xl font-bold mb-3">Blog1</h2>
+       
+        <div className="container mx-auto md:px-10 "> 
+          
 
-          <p>jkhskfhkghrekgj</p>
+          <div className=" md:w-1/3 h-50"></div>
+          <div className="bg-gray-300 rounded-lg w-full md:2/3 p-10">
+            
+           
+            {!isLoading && blog
+              && blog.map((item) => {
+                  return (
+                    <div>
+                      <h4><b>{item.title}</b>  <br></br> {item.description}</h4> {" "}
+                     
+                    </div>
+                  );
+                })
+              }
+          </div>
         </div>
-      </div>
+        
 
-      <Button label=" Create" href="/Newpage/Create" isLink />
-    </section>
+        <Button label=" Create" href="/Newpage/Create" isLink />
+      </section>
+      <Footer />
+    </div>
+    
   );
 };
 
