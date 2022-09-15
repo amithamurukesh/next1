73a1b9b4599f65ac5206610ec2a1554476d50f7a
amithamurukesh <109946862+amithamurukesh@users.noreply.github.com>
2022-09-15 14:55:05 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,19 @@
 export default function Header() {
   return (
-    <header className="bg-violet-300">
+    <header className="bg-violet-300  xl: h-[20%] sm:h-[7%] md:h-[8%]">
       <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
-        <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
-          <input type="text" placeholder="Search..." />
+        <div className=" md:flex-none w-96 order-2  sm:order-2 flex justify-center  py-10 sm:py-2">
+          <input
+            type="text"
+            className="rounded-full text-sm h-6 w-40 "
+            placeholder="Search..."
+          />
         </div>
-        <div className="shrink w-80 sm:order-2">
-          <a className="font-bold uppercasetext-5xl">www.Blogs.com</a>
+        <div className="   shrink w-80 sm:order-2">
+          <a className="font-bold uppercasetext-8xl">www.Blogs.com</a>
         </div>
-        <div className="w-96 order-3 flex justify-center">
-          <div className="flex gap-6">
+        <div className="w-96 order-3 flex justify-center ">
+          <div className="flex gap-6 ">
             <a>Login</a>
             <a>Settings</a>
             <a>Other</a>
