cf32dc3f61df82a47909c1e7606520100951c3e8
amithamurukesh <109946862+amithamurukesh@users.noreply.github.com>
2022-09-15 14:53:08 +0530

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { FunctionComponent } from "react";
-import { RouteProps } from "react-router-dom";
+//import { RouteProps } from "react-router-dom";
 
 /* Define your props */
 interface Props {
@@ -12,7 +12,7 @@ interface Props {
 /* Define component with type  */
 const Button: FunctionComponent<Props> = ({ isLink, label, href }) => {
   return (
-    <div className=" flex items-center justify-center border-2 p-3 w-80 m-8  border-violet text-violet-400 rounded-full  font-semibold hover:bg-violet-500 hover:text-violet-300 text-center">
+    <div className=" absolute inset-x-0 bottom-0 h-14 flex  items-center justify-center border-2 p-3 w-60 m-12 mb-[120px] text-2xl border-violet-600 text-violet-500 rounded-full  font-semibold hover:bg-violet-500 hover:text-gray-300 text-center">
       <Link href={href}>{label}</Link>
     </div>
   );
